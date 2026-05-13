5d1fe895c4e8e24183c26b6eca2a38c4b35f6a35
Anish Pallati <i@anish.land>
2026-05-13 16:15:50 -0400

    fix(storybook-svelte): add vite svelte plugin

--- apps/storybook-svelte/.storybook/main.ts
+++ apps/storybook-svelte/.storybook/main.ts
@@ -2,10 +2,7 @@ import type { StorybookConfig } from "@storybook/svelte-vite";
 
 const config: StorybookConfig = {
     framework: "@storybook/svelte-vite",
-    stories: [
-        "../../../packages/svelte/src/**/*.stories.@(ts|svelte|mdx)",
-        "../src/**/*.stories.@(ts|svelte|mdx)",
-    ],
+    stories: ["../src/**/*.stories.@(ts|svelte|mdx)"],
     addons: ["@storybook/addon-a11y", "@storybook/addon-svelte-csf"],
 };
 
